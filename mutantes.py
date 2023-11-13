c8015820340fe09c6f372cd04c60c0d525282f76
mn204 <mln204manu@gmail.com>
2023-11-13 17:36:13 -0300

    Se realiza la primera parte del programa en donde se pide al usuario la carga de filas y se agregan a la matriz de dna

---
+++ mutantes.py
@@ -0,0 +1,17 @@
+print("--------------------------------------------")
+print("Comienzo del programa mutantes.")
+print("--------------------------------------------")
+
+print("Ingrese las filas de la matriz de ADN:")
+print("Las letras validas son: A, T, C, G.")
+
+dna = []
+for i in range(6):
+    fila = input(f"Fila nro {i + 1}: ").upper()
+    while len(fila) != 6 or any(letra not in "ATCG" for letra in fila):
+        print("Recuerde ingresar exactamente 6 letras válidas (A, T, C, G).")
+        fila = input(f"Fila nro {i + 1}: ").upper()
+    print("Fila agregada correctamente.")
+    dna.append(fila)
+
+
