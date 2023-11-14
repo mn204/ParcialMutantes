c4857b93ac4529f2327fe9e439092949f65d022e
mn204 <mln204manu@gmail.com>
2023-11-14 00:32:05 -0300

    Se agrega la funcion isMutant que recibe el Array de strings de adn y busca las secuencias, se mejora la interfaz, se actualiza readme

--- mutantes.py
+++ mutantes.py
@@ -16,8 +16,45 @@ for i in range(6):
 
 print("--------------------------------------------")
 print("Matriz de ADN ingresada:")
+print("--------------------------------------------")
 for fila in dna:
-    print(" ".join(fila))
+    print("\t".join(fila))
 print("--------------------------------------------")
 
 
+def isMutant(dna):
+    secuencias_encontradas = 0
+
+    for fila in dna: # verifico las horizontales
+        for i in range(len(fila) - 3):
+            if len(set(fila[i:i+4])) == 1:
+                secuencias_encontradas += 1
+                if secuencias_encontradas >= 2:
+                    return True
+
+    for columna in range(len(dna[0])): # verifico las verticales
+        for i in range(len(dna) - 3):
+            if len(set(dna[fila][columna] for fila in range(i, i+4))) == 1:
+                secuencias_encontradas += 1
+                if secuencias_encontradas >= 2:
+                    return True
+
+    for i in range(len(dna) - 3):  #verifico las diagonales
+        for j in range(len(dna[0]) - 3):
+            if len(set(dna[i+k][j+k] for k in range(4))) == 1:
+                secuencias_encontradas += 1
+                if secuencias_encontradas >= 2:
+                    return True
+            if len(set(dna[i+k][j+3-k] for k in range(4))) == 1:
+                secuencias_encontradas += 1
+                if secuencias_encontradas >= 2:
+                    return True
+
+    return secuencias_encontradas >= 2
+
+
+if isMutant(dna):
+    print("El ADN es mutante.")
+else:
+    print("El ADN no es mutante.")
+print("--------------------------------------------")
